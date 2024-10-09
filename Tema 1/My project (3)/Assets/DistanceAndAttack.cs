e8e6f7e62a3ded4db6a7cba07f0ef105d6b4bee8
VatamaneluMario <vatamanelu_narcis@yahoo.ro>
2024-10-09 23:06:55 +0300

    update

--- Tema 1/My project (3)/Assets/DistanceAndAttack.cs
+++ Tema 1/My project (3)/Assets/DistanceAndAttack.cs
@@ -8,16 +8,19 @@ public class DistanceAndAttack : MonoBehaviour
     public GameObject cactus2;
 
     public Animator a1;
+    public Animator a2;
 
     void Update()
     {
         if(Vector3.Distance(cactus1.transform.position, cactus2.transform.position) < 0.25f)
         {
             a1.SetBool("attack", true);
+            a2.SetBool("attack", true);
         }
         else
         {
             a1.SetBool("attack", false);
+            a2.SetBool("attack", false);
         }
     }
 }
